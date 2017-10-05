940114398f0b7fb37e3daaf246abf6b0ca6015d9
Kevin Peña <kevin.pena.prog@gmail.com>
2017-10-05 15:36:28 -0500

    Add the form to the main page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,23 @@ class App extends Component {
   render() {
     return (
       <div>
-
+        <form id="test-form">
+          <div>
+            <label>Name</label>
+            <input type="text" name="name" placeholder="Name"/>
+          </div>
+          <div>
+            <label>Email</label>
+            <input type="text" name="email" placeholder="Email"/>
+          </div>
+          <div>
+            <label>Message</label>
+            <input type="text" name="message" placeholder="Message"/>
+          </div>
+          <div>
+            <button type="submit" id="submit-form">Submit</button>
+          </div>
+        </form>
       </div>
     )
   }
