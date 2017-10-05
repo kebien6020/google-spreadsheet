c753b8db4bfe071591aebb61214555ce8ae8f1d9
Kevin Peña <kevin.pena.prog@gmail.com>
2017-10-05 15:42:26 -0500

    Synchronize form fields with state

--- src/App.js
+++ src/App.js
@@ -2,24 +2,54 @@ import React, { Component } from 'react'
 import './App.css'
 
 class App extends Component {
+  state = {
+    name: '',
+    email: '',
+    message: ''
+  }
+
+  handleChange = event => {
+    const { name, value } = event.target
+    this.setState({[name]: value})
+  }
+
   render() {
+    const { state } = this
     return (
       <div>
-        <form id="test-form">
+        <form>
           <div>
             <label>Name</label>
-            <input type="text" name="name" placeholder="Name"/>
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              value={state.name}
+              onChange={this.handleChange}
+            />
           </div>
           <div>
             <label>Email</label>
-            <input type="text" name="email" placeholder="Email"/>
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              value={state.email}
+              onChange={this.handleChange}
+            />
           </div>
           <div>
             <label>Message</label>
-            <input type="text" name="message" placeholder="Message"/>
+            <input
+              type="text"
+              name="message"
+              placeholder="Message"
+              value={state.message}
+              onChange={this.handleChange}
+            />
           </div>
           <div>
-            <button type="submit" id="submit-form">Submit</button>
+            <button>Submit</button>
           </div>
         </form>
       </div>
