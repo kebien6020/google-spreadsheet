b7e11bb69f4486acc46d7709c39336cbdb117d2e
Kevin Peña <kevin.pena.prog@gmail.com>
2017-10-05 16:06:06 -0500

    Handle the submit

--- src/App.js
+++ src/App.js
@@ -15,8 +15,26 @@ class App extends Component {
     this.setState({[name]: value})
   }
 
-  handleSubmit = () => {
-    console.log(sheetUrl)
+  handleSubmit = async () => {
+    // Converts {a:1,b:2} into 'a=1&b=2'
+    const objToParams = obj =>
+      Object.keys(obj)
+        .map(key => [key, obj[key]])
+        .map(([key, val]) => `${key}=${val}`)
+        .join('&')
+
+    // Serialize the state, ie. the form fields as query params
+    const queryParams = '?' + objToParams(this.state)
+
+    const response = await fetch(sheetUrl + queryParams)
+
+    // If everything went well we are done, parse and log the response
+    // just in case.
+    // In a real app we would check the response to see that everything
+    // went fine and show an error otherwise
+    const result = await response.json()
+
+    console.log(result)
   }
 
   render() {
